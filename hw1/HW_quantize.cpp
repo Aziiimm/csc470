6b94a275f80972b4f3c8f25dcc49bb49e7233813
Azim Rahat <azimrahat108@gmail.com>
2025-09-12 12:22:13 -0400

    started hw1

---
+++ hw1/HW_quantize.cpp
@@ -0,0 +1,16 @@
+#include "IP.h"
+using namespace IP;
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// HW_quantize:
+//
+// Quantize I1 to specified number of levels. Apply dither if flag is set.
+// Output is in I2.
+//
+void
+HW_quantize(ImagePtr I1, int levels, bool dither, ImagePtr I2)
+{
+
+// PUT YOUR CODE HERE
+
+}
